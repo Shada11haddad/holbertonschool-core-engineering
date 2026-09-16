6cfb5609ca12502039b6f0105828270b4c541bb2
Shada11haddad <shada.hadad123@gmail.com>
2026-09-16 23:48:47 +0300

    first task

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+languge = "Python"
+version = 3
+pi_approx = 3.123423
+compu =( 7 > 4)
+
+print(f"Language: {languge}")
+print(f"Version: {version}")
+print(f"Pi approx: {pi_approx:.2f}")
+print(f"Computation Valid: {compu}")
