5949ea3da21a2d05d7d5609b6c053bf303dfa5f7
Shada11haddad <shada.hadad123@gmail.com>
2026-09-17 17:30:53 +0300

    ll

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3  
-
+#!/usr/bin/env python3
 
 for letter in "abcdefghijklmnopqrstuvwxyz":
-	if letter != 'e' and letter != 'q':
-		print(letter, end="\n" if letter == 'z' else "")
+    if letter != 'e' and letter != 'q':
+        print(letter, end="\n" if letter == 'z' else "")
