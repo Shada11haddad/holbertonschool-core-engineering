eb4c91dd28364f7c63fa2f7e1addbc5f95935aa0
Shada11haddad <shada.hadad123@gmail.com>
2026-09-26 10:22:10 +0300

    Add __str__ to Square

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Defines a Square class, a subclass of Rectangle."""
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square, a rectangle with equal width and height."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return the square description: [Square] <width>/<height>."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
